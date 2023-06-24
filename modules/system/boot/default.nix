14acb5875a0db755bf016e84ce8795e1cc7a2811
atopian <github@atopian.com>
2023-06-23 22:36:54 -0400

    Cant touch EFI

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -12,7 +12,7 @@ in
    config = mkIf cfg.enable {
       boot.loader.systemd-boot.enable = true;
       boot.loader.systemd-boot.configurationLimit = 5;
-      boot.loader.efi.canTouchEfiVariables = true;
+      boot.loader.efi.canTouchEfiVariables = false;
 
       # https://github.com/NixOS/nixpkgs/blob/c32c39d6f3b1fe6514598fa40ad2cf9ce22c3fb7/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix#L66
       boot.loader.systemd-boot.editor = false;
