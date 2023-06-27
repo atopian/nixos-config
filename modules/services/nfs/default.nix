0df534ddf7ef41ebd5cdb358dbb1a04e8825442d
atopian <github@atopian.com>
2023-06-27 09:14:18 -0400

    nfs

--- modules/services/nfs/default.nix
+++ modules/services/nfs/default.nix
@@ -6,7 +6,7 @@ let
    cfg = config.fruitcellar.services.nfs;
 in
 {
-   options.fruitcellar.services.ssh = with types; {
+   options.fruitcellar.services.nfs = with types; {
       enable = mkBoolOpt false "Enable NFS Server";
    };
 
